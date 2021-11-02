7a722149d303ebc6b9e3971b4f3034d9071a252c
Dayelle Souza <44237050+dayellesouza@users.noreply.github.com>
2021-11-02 20:23:19 -0300

    First Badge commit :tada: :rocket:

---
+++ main.js
@@ -0,0 +1,45 @@
+const linksSocialMedia = {
+  github: 'dayellesouza',
+  youtube: 'dayelle14',
+  facebook: 'dayelleantonia',
+  instagram: 'daaysweet_',
+  twitter: 'we_daay'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      ;(userName.textContent = data.name),
+        (userBio.textContent = data.bio),
+        (userLink.href = data.html_url),
+        (userImage.src = data.avatar_url),
+        (userLogin.textContent = data.login)
+    })
+}
+
+getGitHubProfileInfos()
+
+function setTitlePage() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      title.textContent = data.name + " - DoWhile 2021"
+    })
+}
+
+setTitlePage()
